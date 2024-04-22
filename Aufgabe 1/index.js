780e0b493203171123e94a520f3d4b56b3c48310
sonkeks <Soenke.Schaarschmidt@Student.HTW-Berlin.de>
2024-04-22 15:30:03 +0200

    csv generation & add rounds

--- Aufgabe 1/index.js
+++ Aufgabe 1/index.js
@@ -1,22 +1,24 @@
 let grid = document.getElementById("grid");
 let experimentData = [];
-let experimentID = 0;
+let experimentID = Math.random().toString(36).slice(2, 9);
 let successfulRound = true;
-let successfulRounds = 0;
 let start;
-let end;
 
 
-// 1 Runde alles schwarz
-let round_1 = {"round_number": 0, "direction": "left", "color": ["black"], "searched_color": "black", "grid_size": 10, "positionX": 3, "positionY": 2}
-// 2 Runde Farben nah beieinander, es darf nur eine Kombination von Farbe + Richtung richtig sein
-let round_2 = {"round_number": 1, "direction": "down", "color": ["darkred"], "searched_color": "red", "grid_size": 10, "positionX": 4, "positionY": 3}
+// 1-2 Runde alles schwarz
+let round_1 = {"round_number": 1, "direction": "left", "color": ["#36453B"], "searched_color": "#36453B", "grid_size": 8, "positionX": 3, "positionY": 2}
+let round_2 = {"round_number": 2, "direction": "down", "color": ["#36453B"], "searched_color": "#36453B", "grid_size": 8, "positionX": 1, "positionY": 2}
+// 3-4 Runde Farben nah beieinander, es gibt ein Signalelement
+let round_3 = {"round_number": 3, "direction": "up", "color": ["#36453B"], "searched_color": "#CBEF43", "grid_size": 8, "positionX": 4, "positionY": 3}
+let round_4 = {"round_number": 4, "direction": "left", "color": ["#36453B"], "searched_color": "#CBEF43", "grid_size": 8, "positionX": 6, "positionY": 7}
+// 5-6 Farben nah beieinander, es darf nur eine Kombination von Farbe + Richtung richtig sein
+let round_5 = {"round_number": 5, "direction": "right", "color": ["#36453B", "#CBEF43"], "searched_color": "#CBEF43", "grid_size": 8, "positionX": 2, "positionY": 4}
+let round_6 = {"round_number": 6, "direction": "up", "color": ["#36453B", "#CBEF43"], "searched_color": "#CBEF43", "grid_size": 8, "positionX": 7, "positionY": 7}
+// 7-8 Runde wie Runde 2 aber Starker Farbkontrast
+let round_7 = {"round_number": 7, "direction": "right", "color": ["#36453B", "#cbef43", "#2ec4b6", "#e94f37"], "searched_color": "#e94f37", "grid_size": 8, "positionX": 5, "positionY": 1}
+let round_8 = {"round_number": 8, "direction": "down", "color": ["#36453B", "#cbef43", "#2ec4b6", "#e94f37"], "searched_color": "#cbef43", "grid_size": 8, "positionX": 4, "positionY": 4}
 
-let round_3 = {"round_number": 2, "direction": "up", "color": ["darkred", "red"], "searched_color": "red", "grid_size": 10, "positionX": 1, "positionY": 4}
-// 3 Runde wie Runde 2 aber Starker Farbkontrast
-let round_4 = {"round_number": 3, "direction": "right", "color": ["green", "blue", "yellow", "red"], "searched_color": "red", "grid_size": 10, "positionX": 4, "positionY": 3}
-
-let rounds = [round_1, round_2, round_3, round_4]
+let rounds = [round_1, round_2, round_3, round_4, round_5, round_6, round_7, round_8];
 let current_round = 0;
 
 setUpRound(rounds[current_round])
@@ -80,21 +82,20 @@ function generateGrid(round) {
 
 async function handleClick(event, isCorrectPosition) {
     if (isCorrectPosition) {
-        console.log("clicked correctly");
+        //console.log("clicked correctly");
         successfulRound = true;
-        collectData(current_round);
+        collectData();
     } else {
-        console.log("you are wrong");
+        //console.log("you are wrong");
         successfulRound = false;
-          collectData(current_round);
+        collectData();
     }
     if(current_round < rounds.length -1){
         current_round++;
         setUpRound(rounds[current_round]);
     } else {
         //show results;
-        console.log("experiment done!");
-        summarizeData(experimentData);
+        summarizeData();
         endExperiment();
 
     }
@@ -120,7 +121,6 @@ function generateCell(round, isCorrectPosition) {
 
 function getRandomDirection(directionToAvoid, searched_color, color) {
     const directions = ["left", "up", "right", "down"];
-    //const filteredDirections = directions.filter(direction => direction !== directionToAvoid);
     let randomIndex = Math.floor(Math.random() * directions.length);
     let direction = directions[randomIndex];
     let randomColorValue = Math.floor(Math.random() * (color.length - 1 + 1));
@@ -163,63 +163,74 @@ function createSvg() {
 }
 
 
-function collectData(round) {
-  console.log("current round: " + current_round);
+function collectData() {
+  //console.log("current round: " + current_round);
   let end = Date.now();
-  let time = (((end - start) - 4000) / 1000);
-  console.log("time: " + time + "s");
-
-
-   let currentExperiment = {
-      experimentID: experimentID,
-      rounds: []
-    };
-    experimentData.push(currentExperiment);
-
+  let time = (((end - start) - 3050) / 1000);
+  //console.log("time: " + time + "s");
 
   // Prepare data object for the current round
   let roundData = {
-    round: current_round,
+    roundNr: current_round,
     reactionTime: time,
-    success: successfulRound
+    success: successfulRound,
   };
 
   // Add round data to the current experiment
-  currentExperiment.rounds.push(roundData);
+  experimentData.push(roundData);
 
-  // Log experiment data to console
-  console.log(experimentData);
 }
-function summarizeData(data) {
+function summarizeData() {
   let totalReactionTime = 0;
   let longestReactionTime = 0;
-  let shortestReactionTime = 0;
+  let shortestReactionTime = 100000;
 
+  let csvFileData = {
+      'Experiment ID': experimentID,
+      'Successful Rounds': 0,
+      'Failed Rounds': 0,
+  };
 
-  for (let experiment of data) {
-
-    for (let round of experiment.rounds) {
-        console.log("round success: " + round.success);
+  for (let round of experimentData) {
+      //console.log("round success: " + round.success);
       if (round.success === true) {
-        successfulRounds++;
+          csvFileData['Successful Rounds'] += 1;
+      } else {
+          csvFileData['Failed Rounds'] += 1;
       }
-      totalReactionTime += round.reactionTime;
+      let roundKey = `Round ${round.roundNr + 1} Reaction Time:`;
+      csvFileData[roundKey] = round.reactionTime.toFixed(2) + "s";
 
+      totalReactionTime += round.reactionTime;
       if (round.reactionTime > longestReactionTime) {
         longestReactionTime = round.reactionTime;
       }
       if (round.reactionTime < shortestReactionTime) {
             shortestReactionTime = round.reactionTime;
       }
-    }
   }
 
   let averageReactionTime = totalReactionTime / rounds.length;
+  csvFileData['Longest Reaction Time:'] = longestReactionTime.toFixed(4) + "s";
+  csvFileData['Shortest Reaction Time:'] = shortestReactionTime.toFixed(4) + "s";
+  csvFileData['Average Reaction Time:'] = averageReactionTime.toFixed(4) + "s";
+  csvFileData['Total Reaction Time:'] = totalReactionTime.toFixed(4) + "s";
+  csvDownload(csvFileData);
+}
 
-  console.log("Analysis of Experiment Data:");
-  console.log("Successful Rounds:", successfulRounds);
-  console.log("Total Reaction Time:", totalReactionTime.toFixed(2) + "s");
-  console.log("Average Reaction Time:", averageReactionTime.toFixed(2) + "s");
-  console.log("Longest Reaction Time:", longestReactionTime.toFixed(2) + "s");
-  console.log("Shortest Reaction Time:", shortestReactionTime.toFixed(2) + "s");
+function csvDownload(csvFileData) {
+    let csvRows = "";
+    let keys = Object.keys(csvFileData);
+    keys.forEach((key) => {
+        const value = csvFileData[key];
+        csvRows += `${key},${value}\n`;
+    })
+
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvRows);
+    hiddenElement.target = '_blank';
+
+    //provide the name for the CSV file to be downloaded
+    hiddenElement.download = 'Results.csv';
+    hiddenElement.click();
 }
